70bcddef5d662f1877b917bfa7345a7fe7b3acc0
Orace EDJO <oracejuscard@gmail.com>
2024-04-29 14:51:28 +0200

    mise à jour env

--- databases/dbConfig.js
+++ databases/dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'otrip'
+    database: process.env.DB_NAME || 'madb'
   };
   
   
